331c7e8a8407364daaa15d5803fe8b8c453ad0ea
jackie lee <catiga03@gmail.com>
2023-08-26 11:56:20 +0800

    p

--- target/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-HOME="/data/dcw-regulustar"
-ENV="prod"
-
-export DUBBO_GO_CONFIG_PATH="$HOME/conf/dubbogo-$ENV.yaml"
-export DALINK_GO_CONFIG_PATH="$HOME/conf/param-$ENV.yaml"
-
-nohup ./regulustar-linux &
